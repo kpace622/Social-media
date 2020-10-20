4017305246d25842f24e5ddcb299e1374feb0788
Kyle Pace <kpace2296@gmail.com>
2020-10-20 11:27:56 -0500

    using yarn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+// import firebase from 'firebase'
 
 function App() {
   return (
     <div className="App">
-
+      {console.log(firebase)}
     </div>
   );
 }
